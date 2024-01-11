b89c77e2a5d92ecfa1a671a3f65a0959cb24aaff
unknown <ryun9593@naver.com>
2024-01-11 16:41:46 +0900

    tempAdc

---
+++ Core/Src/uart.c
@@ -0,0 +1,18 @@
+/*
+ * uart.c
+ *
+ *  Created on: Jan 10, 2024
+ *      Author: npedu-203-15
+ */
+
+#include "uart.h"
+
+UART_HandleTypeDef *huart;
+
+void initUart(UART_HandleTypeDef *inHuart){
+	huart = inHuart;
+}
+int _write(int file, char *p, int len){
+	HAL_UART_Transmit(huart, (uint8_t *)p, len, len);
+	return len;
+}
